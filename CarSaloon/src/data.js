0216d7ed10e0c2f8865a0133c4c2f6eb01eaff21
UvejsMurtezi <um30025@seeu.edu.mk>
2025-02-01 18:10:10 +0100

    car models selected

--- CarSaloon/src/data.js
+++ CarSaloon/src/data.js
@@ -1,54 +1,32 @@
 // src/data.js
 export const carsData = [
-    {
-      make: "Mercedes",
-      model: "S-Class",
-      year: "2023",
-      price: "50.000",
-      glb: "Mercedes.glb",
-    },
-    { make: "Audi", model: "Q5", year: "2022", price: "35.000", glb: "audi.glb" },
-    {
-      make: "Volvo",
-      model: "s60",
-      year: "2017",
-      price: "20.000",
-      glb: "s60.glb",
-    },
-    {
-      make: "Maserati",
-      model: "Quattroporte",
-      year: "2020",
-      price: "30.000",
-      glb: "maserati.glb",
-    },
-    {
-      make: "BMW",
-      model: "M4",
-      year: "2018",
-      price: "23.000",
-      glb: "M4.glb",
-    },
-    {
-      make: "Tesla",
-      model: "Model 3",
-      year: "2021",
-      price: "20.000",
-      glb: "tesla.glb",
-    },
-    {
-      make: "Skoda",
-      model: "Superb",
-      year: "2016",
-      price: "10.000",
-      glb: "skoda_superb.glb",
-    },
-    {
-      make: "Kia",
-      model: "Stonic",
-      year: "2016",
-      price: "13.000",
-      glb: "kia_stonic.glb",
-    },
-  ];
-  
+  {
+    make: "Mercedes",
+    model: "S-Class",
+    year: "2023",
+    price: "50.000",
+    glb: "Mercedes.glb",
+  },
+  { make: "Audi", model: "Q5", year: "2022", price: "35.000", glb: "audi.glb" },
+  {
+    make: "Volvo",
+    model: "s60",
+    year: "2017",
+    price: "20.000",
+    glb: "s60.glb",
+  },
+  {
+    make: "Maserati",
+    model: "Quattroporte",
+    year: "2020",
+    price: "30.000",
+    glb: "maserati.glb",
+  },
+  {
+    make: "Skoda",
+    model: "Superb",
+    year: "2016",
+    price: "10.000",
+    glb: "skoda_superb.glb",
+  },
+];
